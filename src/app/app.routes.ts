59c245405720bbdb0bd44d5a4e2f646aa7e8ccba
unknown <bmoszczynski2007@gmail.com>
2024-09-03 10:40:38 +0200

    designing not found page and adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
