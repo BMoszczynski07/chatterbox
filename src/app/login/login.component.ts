342d7f9d13f4b391e85b8f84d830ff06d52999b8
unknown <bmoszczynski2007@gmail.com>
2024-09-04 10:00:32 +0200

    designing login page 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  err: boolean = false;
+
+  form = {
+    username: '',
+    password: '',
+  };
+
+  handleSubmitForm = (e: Event) => {
+    e.preventDefault();
+
+    if (this.form.username === '' || this.form.password === '') return;
+    console.log('submit');
+  };
+}
